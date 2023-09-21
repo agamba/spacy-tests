c01b4138b2952cd5fa2aa3ab4e83f35d4b2c2194
agamba <lodeanto@gmail.com>
2023-09-21 08:14:22 -0500

    added license and small changes to subject_object_extraction.py

--- subject_object_extraction.py
+++ subject_object_extraction.py
@@ -31,6 +31,7 @@ import spacy
 SUBJECTS = ["nsubj", "nsubjpass", "csubj", "csubjpass", "agent", "expl"]
 OBJECTS = ["pobj", "dobj", "dative", "attr", "oprd"]
 
+
 def getSubsFromConjunctions(subs):
     moreSubs = []
     for sub in subs:
@@ -38,10 +39,12 @@ def getSubsFromConjunctions(subs):
         rights = list(sub.rights)
         rightDeps = {tok.lower_ for tok in rights}
         if "and" in rightDeps:
-            moreSubs.extend([tok for tok in rights if tok.dep_ in SUBJECTS or tok.pos_ == "NOUN"])
+            moreSubs.extend(
+                [tok for tok in rights if tok.dep_ in SUBJECTS or tok.pos_ == "NOUN"])
             if len(moreSubs) > 0:
                 moreSubs.extend(getSubsFromConjunctions(moreSubs))
     return moreSubs
+
 
 def getObjsFromConjunctions(objs):
     moreObjs = []
@@ -50,20 +53,24 @@ def getObjsFromConjunctions(objs):
         rights = list(obj.rights)
         rightDeps = {tok.lower_ for tok in rights}
         if "and" in rightDeps:
-            moreObjs.extend([tok for tok in rights if tok.dep_ in OBJECTS or tok.pos_ == "NOUN"])
+            moreObjs.extend(
+                [tok for tok in rights if tok.dep_ in OBJECTS or tok.pos_ == "NOUN"])
             if len(moreObjs) > 0:
                 moreObjs.extend(getObjsFromConjunctions(moreObjs))
     return moreObjs
+
 
 def getVerbsFromConjunctions(verbs):
     moreVerbs = []
     for verb in verbs:
         rightDeps = {tok.lower_ for tok in verb.rights}
         if "and" in rightDeps:
-            moreVerbs.extend([tok for tok in verb.rights if tok.pos_ == "VERB"])
+            moreVerbs.extend(
+                [tok for tok in verb.rights if tok.pos_ == "VERB"])
             if len(moreVerbs) > 0:
                 moreVerbs.extend(getVerbsFromConjunctions(moreVerbs))
     return moreVerbs
+
 
 def findSubs(tok):
     head = tok.head
@@ -81,12 +88,14 @@ def findSubs(tok):
         return [head], isNegated(tok)
     return [], False
 
+
 def isNegated(tok):
     negations = {"no", "not", "n't", "never", "none"}
     for dep in list(tok.lefts) + list(tok.rights):
         if dep.lower_ in negations:
             return True
     return False
+
 
 def findSVs(tokens):
     svs = []
@@ -95,15 +104,19 @@ def findSVs(tokens):
         subs, verbNegated = getAllSubs(v)
         if len(subs) > 0:
             for sub in subs:
-                svs.append((sub.orth_, "!" + v.orth_ if verbNegated else v.orth_))
+                svs.append(
+                    (sub.orth_, "!" + v.orth_ if verbNegated else v.orth_))
     return svs
+
 
 def getObjsFromPrepositions(deps):
     objs = []
     for dep in deps:
         if dep.pos_ == "ADP" and dep.dep_ == "prep":
-            objs.extend([tok for tok in dep.rights if tok.dep_  in OBJECTS or (tok.pos_ == "PRON" and tok.lower_ == "me")])
+            objs.extend([tok for tok in dep.rights if tok.dep_ in OBJECTS or (
+                tok.pos_ == "PRON" and tok.lower_ == "me")])
     return objs
+
 
 def getObjsFromAttrs(deps):
     for dep in deps:
@@ -118,6 +131,7 @@ def getObjsFromAttrs(deps):
                         return v, objs
     return None, None
 
+
 def getObjFromXComp(deps):
     for dep in deps:
         if dep.pos_ == "VERB" and dep.dep_ == "xcomp":
@@ -129,6 +143,7 @@ def getObjFromXComp(deps):
                 return v, objs
     return None, None
 
+
 def getAllSubs(v):
     verbNegated = isNegated(v)
     subs = [tok for tok in v.lefts if tok.dep_ in SUBJECTS and tok.pos_ != "DET"]
@@ -139,14 +154,15 @@ def getAllSubs(v):
         subs.extend(foundSubs)
     return subs, verbNegated
 
+
 def getAllObjs(v):
     # rights is a generator
     rights = list(v.rights)
     objs = [tok for tok in rights if tok.dep_ in OBJECTS]
     objs.extend(getObjsFromPrepositions(rights))
 
-    #potentialNewVerb, potentialNewObjs = getObjsFromAttrs(rights)
-    #if potentialNewVerb is not None and potentialNewObjs is not None and len(potentialNewObjs) > 0:
+    # potentialNewVerb, potentialNewObjs = getObjsFromAttrs(rights)
+    # if potentialNewVerb is not None and potentialNewObjs is not None and len(potentialNewObjs) > 0:
     #    objs.extend(potentialNewObjs)
     #    v = potentialNewVerb
 
@@ -157,6 +173,7 @@ def getAllObjs(v):
     if len(objs) > 0:
         objs.extend(getObjsFromConjunctions(objs))
     return v, objs
+
 
 def findSVOs(tokens):
     svos = []
@@ -169,29 +186,39 @@ def findSVOs(tokens):
             for sub in subs:
                 for obj in objs:
                     objNegated = isNegated(obj)
-                    svos.append((sub.lower_, "!" + v.lower_ if verbNegated or objNegated else v.lower_, obj.lower_))
+                    svos.append(
+                        (sub.lower_, "!" + v.lower_ if verbNegated or objNegated else v.lower_, obj.lower_))
     return svos
 
+
 def getAbuserOntoVictimSVOs(tokens):
-    maleAbuser = {'he', 'boyfriend', 'bf', 'father', 'dad', 'husband', 'brother', 'man'}
-    femaleAbuser = {'she', 'girlfriend', 'gf', 'mother', 'mom', 'wife', 'sister', 'woman'}
-    neutralAbuser = {'pastor', 'abuser', 'offender', 'ex', 'x', 'lover', 'church', 'they'}
-    victim = {'me', 'sister', 'brother', 'child', 'kid', 'baby', 'friend', 'her', 'him', 'man', 'woman'}
+    maleAbuser = {'he', 'boyfriend', 'bf', 'father',
+                  'dad', 'husband', 'brother', 'man'}
+    femaleAbuser = {'she', 'girlfriend', 'gf',
+                    'mother', 'mom', 'wife', 'sister', 'woman'}
+    neutralAbuser = {'pastor', 'abuser', 'offender',
+                     'ex', 'x', 'lover', 'church', 'they'}
+    victim = {'me', 'sister', 'brother', 'child', 'kid',
+              'baby', 'friend', 'her', 'him', 'man', 'woman'}
 
     svos = findSVOs(tokens)
     wnl = WordNetLemmatizer()
     passed = []
     for s, v, o in svos:
         s = wnl.lemmatize(s)
-        v = "!" + wnl.lemmatize(v[1:], 'v') if v[0] == "!" else wnl.lemmatize(v, 'v')
+        v = "!" + \
+            wnl.lemmatize(v[1:], 'v') if v[0] == "!" else wnl.lemmatize(v, 'v')
         o = "!" + wnl.lemmatize(o[1:]) if o[0] == "!" else wnl.lemmatize(o)
         if s in maleAbuser.union(femaleAbuser).union(neutralAbuser) and o in victim:
             passed.append((s, v, o))
     return passed
 
+
 def printDeps(toks):
     for tok in toks:
-        print(tok.orth_, tok.dep_, tok.pos_, tok.head.orth_, [t.orth_ for t in tok.lefts], [t.orth_ for t in tok.rights])
+        print(tok.orth_, tok.dep_, tok.pos_, tok.head.orth_, [
+              t.orth_ for t in tok.lefts], [t.orth_ for t in tok.rights])
+
 
 def testSVOs():
     # nlp = English()
@@ -200,7 +227,8 @@ def testSVOs():
     tok = nlp("making $12 an hour? where am i going to go? i have no other financial assistance available and he certainly won't provide support.")
     svos = findSVOs(tok)
     printDeps(tok)
-    assert set(svos) == {('i', '!have', 'assistance'), ('he', '!provide', 'support')}
+    assert set(svos) == {('i', '!have', 'assistance'),
+                         ('he', '!provide', 'support')}
     print(svos)
 
     tok = nlp("i don't have other assistance")
@@ -216,18 +244,22 @@ def testSVOs():
     assert set(svos) == {('they', 'ate', 'pizza')}
 
     print("--------------------------------------------------")
-    tok = nlp("I have no other financial assistance available and he certainly won't provide support.")
+    tok = nlp(
+        "I have no other financial assistance available and he certainly won't provide support.")
     svos = findSVOs(tok)
     printDeps(tok)
     print(svos)
-    assert set(svos) == {('i', '!have', 'assistance'), ('he', '!provide', 'support')}
+    assert set(svos) == {('i', '!have', 'assistance'),
+                         ('he', '!provide', 'support')}
 
     print("--------------------------------------------------")
-    tok = nlp("I have no other financial assistance available, and he certainly won't provide support.")
+    tok = nlp(
+        "I have no other financial assistance available, and he certainly won't provide support.")
     svos = findSVOs(tok)
     printDeps(tok)
     print(svos)
-    assert set(svos) == {('i', '!have', 'assistance'), ('he', '!provide', 'support')}
+    assert set(svos) == {('i', '!have', 'assistance'),
+                         ('he', '!provide', 'support')}
 
     print("--------------------------------------------------")
     tok = nlp("he did not kill me")
@@ -236,12 +268,12 @@ def testSVOs():
     print(svos)
     assert set(svos) == {('he', '!kill', 'me')}
 
-    #print("--------------------------------------------------")
-    #tok = nlp("he is an evil man that hurt my child and sister")
-    #svos = findSVOs(tok)
-    #printDeps(tok)
-    #print(svos)
-    #assert set(svos) == {('he', 'hurt', 'child'), ('he', 'hurt', 'sister'), ('man', 'hurt', 'child'), ('man', 'hurt', 'sister')}
+    # print("--------------------------------------------------")
+    # tok = nlp("he is an evil man that hurt my child and sister")
+    # svos = findSVOs(tok)
+    # printDeps(tok)
+    # print(svos)
+    # assert set(svos) == {('he', 'hurt', 'child'), ('he', 'hurt', 'sister'), ('man', 'hurt', 'child'), ('man', 'hurt', 'sister')}
 
     print("--------------------------------------------------")
     tok = nlp("he told me i would die alone with nothing but my career someday")
@@ -258,7 +290,8 @@ def testSVOs():
     assert set(svos) == {('i', 'kill', 'him')}
 
     print("--------------------------------------------------")
-    tok = nlp("because he hit me and also made me so angry i wanted to kill him with a hammer.")
+    tok = nlp(
+        "because he hit me and also made me so angry i wanted to kill him with a hammer.")
     svos = findSVOs(tok)
     printDeps(tok)
     print(svos)
@@ -276,21 +309,24 @@ def testSVOs():
     svos = findSVOs(tok)
     printDeps(tok)
     print(svos)
-    assert set(svos) == {('he', 'shot', 'me'), ('he', 'shot', 'sister'), ('brother', 'shot', 'me'), ('brother', 'shot', 'sister')}
+    assert set(svos) == {('he', 'shot', 'me'), ('he', 'shot', 'sister'),
+                         ('brother', 'shot', 'me'), ('brother', 'shot', 'sister')}
 
     print("--------------------------------------------------")
     tok = nlp("the annoying person that was my boyfriend hit me")
     svos = findSVOs(tok)
     printDeps(tok)
     print(svos)
-    assert set(svos) == {('person', 'was', 'boyfriend'), ('person', 'hit', 'me')}
+    assert set(svos) == {('person', 'was', 'boyfriend'),
+                         ('person', 'hit', 'me')}
 
     print("--------------------------------------------------")
     tok = nlp("the boy raced the girl who had a hat that had spots.")
     svos = findSVOs(tok)
     printDeps(tok)
     print(svos)
-    assert set(svos) == {('boy', 'raced', 'girl'), ('who', 'had', 'hat'), ('hat', 'had', 'spots')}
+    assert set(svos) == {('boy', 'raced', 'girl'),
+                         ('who', 'had', 'hat'), ('hat', 'had', 'spots')}
 
     print("--------------------------------------------------")
     tok = nlp("he spit on me")
@@ -311,14 +347,16 @@ def testSVOs():
     svos = findSVOs(tok)
     printDeps(tok)
     print(svos)
-    assert set(svos) == {('boy', 'raced', 'girl'), ('who', 'had', 'hat'), ('hat', '!have', 'spots')}
+    assert set(svos) == {('boy', 'raced', 'girl'),
+                         ('who', 'had', 'hat'), ('hat', '!have', 'spots')}
 
     print("--------------------------------------------------")
     tok = nlp("he is a nice man that didn't hurt my child and sister")
     svos = findSVOs(tok)
     printDeps(tok)
     print(svos)
-    assert set(svos) == {('he', 'is', 'man'), ('man', '!hurt', 'child'), ('man', '!hurt', 'sister')}
+    assert set(svos) == {('he', 'is', 'man'),
+                         ('man', '!hurt', 'child'), ('man', '!hurt', 'sister')}
 
     print("--------------------------------------------------")
     tok = nlp("he didn't spit on me and my child")
@@ -334,8 +372,8 @@ def testSVOs():
     print(svos)
     # tok = nlp("he beat and hurt me")
 
-def main():
-    testSVOs()
+# def main():
+#     testSVOs()
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
